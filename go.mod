c96ebb134eac3a37fc2e91ea022087a7ffc81cf8
clarkk <clark@electrobeat.dk>
2025-03-07 14:58:36 +0000

    timef.Date()

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.24.0
 require (
 	github.com/clarkk/go-util v0.0.0-20250302110437-f7a532cdaee3
 	github.com/go-errors/errors v1.5.1
-	golang.org/x/sys v0.30.0
+	golang.org/x/sys v0.31.0
 )
--- timef/timef.go
+++ timef/timef.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+func Date(i uint64) string {
+	t := time.Unix(int64(i), 0)
+	return fmt.Sprintf("%02d-%02d-%d", t.Day(), t.Month(), t.Year())
+}
+
 func File() string {
 	now := time.Now()
 	return fmt.Sprintf("%d%02d%02d_%02d%02d%02d.%09d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), now.Nanosecond())
