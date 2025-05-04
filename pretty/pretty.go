37e1e8e81f73f1223ad4f1ec157836405740b064
clarkk <clark@electrobeat.dk>
2025-05-04 17:43:31 +0000

    pretty

--- pretty/pretty.go
+++ pretty/pretty.go
@@ -1,12 +1,17 @@
 package pretty
 
 import (
+	"fmt"
 	"github.com/go-json-experiment/json"
 	"github.com/go-json-experiment/json/jsontext"
 )
 
 func Print(data any){
+	fmt.Println(String(data))
+}
+
+func String(data any) string {
 	b, _ := json.Marshal(data)
 	(*jsontext.Value)(&b).Indent()
-	fmt.Println(string(b))
+	return string(b)
 }
